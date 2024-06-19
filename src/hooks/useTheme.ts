1fae8ebdd15b8b954c5bfc4e7d9d496f55e0b370
end0rfin <bludovcom@yandex.ru>
2024-06-19 11:21:18 +0300

    feat(theme): add mood and warm theme

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,17 +1,22 @@
 import { useEffect, useState } from "react";
 
+export enum Theme {
+  mood = "mood",
+  warm = "warm",
+}
+
 export const useTheme = () => {
-  const { 0: theme, 1: setTheme } = useState("light");
+  const { 0: theme, 1: setTheme } = useState(Theme.mood);
 
   const toggleTheme = () => {
-    const toggledTheme = theme !== "dark" ? "dark" : "light";
+    const toggledTheme = theme !== Theme.warm ? Theme.warm : Theme.mood;
 
     localStorage.setItem("theme", toggledTheme);
     setTheme(toggledTheme);
   };
 
   useEffect(() => {
-    const localTheme = localStorage.getItem("theme");
+    const localTheme = localStorage.getItem("theme") as Theme;
 
     if (localTheme) {
       setTheme(localTheme);
