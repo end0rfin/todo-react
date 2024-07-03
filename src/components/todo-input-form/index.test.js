7e102a7d0057783ab637c5c4f9f281adb81aa043
end0rfin <bludovcom@yandex.ru>
2024-07-03 08:44:45 +0300

    feat(jest): add and config jest tests

---
+++ src/components/todo-input-form/index.test.js
@@ -0,0 +1,24 @@
+import { describe, expect, test } from "@jest/globals";
+// import { render, screen } from "@testing-library/react";
+// import userEvent from "@testing-library/user-event";
+// import "@testing-library/jest-dom/extend-expect";
+// import App from "./App";
+
+// test("User can remove item from page", () => {
+//   render(<App />);
+//   const listItemInput = screen.getByLabelText(/Create List Item/i);
+//   const addItemButton = screen.getByTestId("add-item");
+//
+//   userEvent.type(listItemInput, "hello");
+//   userEvent.click(addItemButton);
+//   const newItem = screen.getByText("hello");
+//   expect(newItem).toBeInTheDocument();
+//
+//   const removeButton = screen.getByTestId("remove-item0");
+//   userEvent.click(removeButton);
+//   expect(newItem).not.toBeInTheDocument();
+// });
+
+test("Todo input form", () => {
+  console.log("Running jest test");
+});
